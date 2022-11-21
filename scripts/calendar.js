702629b7aa89ccc1310f0e09ec7190bbd9355119
Ethan Quinn Vince-Budan <vincebue@mcmaster.ca>
2022-11-20 22:20:54 -0500

    Moved events to local var; formatting changes

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,7 +1,33 @@
 const monthNames = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const eventsLocation = "../calendarEvents.json";
 const tasksLocation = "../tasksEvents.json";
+
+const events = [
+	{
+		"name": "Class",
+		"person": "Ethan",
+		"date": "2022-11-05",
+		"times": [6,7,8]
+	},
+	{
+		"name": "Class",
+		"person": "Namit",
+		"date": "2022-11-05",
+		"times": [7,8,9,10]
+	},
+	{
+		"name": "Private event",
+		"person": "Justin",
+		"date": "2022-11-05",
+		"times": [8,9]
+	},
+	{
+		"name": "Dinner",
+		"person": "Janek",
+		"date": "2022-11-05",
+		"times": [18,19]
+	}
+]
 
 let currMonth = new Date().getMonth();
 let currYear = new Date().getFullYear();
@@ -26,6 +52,7 @@ function buildCalendar() {
 	let startDay = new Date(workingYear,workingMonth, 1).getDay();
 	let numDays = getDaysInMonth(workingYear, workingMonth);
 	let result = "<tbody>";
+  let isSelected = selectedDate.year == workingYear && selectedDate.month == workingMonth;
 	let numRows = (startDay + numDays)/7;
 	for (let i = 0; i < numRows; i++) {
 		result += "<tr>";
@@ -34,7 +61,11 @@ function buildCalendar() {
 			if (dayNum <= 0 || dayNum > numDays) {
 				result += '<td class="table-secondary"></td>';
 			} else {
-				result += "<td><button type='button' class='btn btn-light' onClick='selectNewDay(" + "workingYear, workingMonth, " + dayNum + ")'>" + dayNum + "</button></td>";
+        if (isSelected && selectedDate.day == dayNum) {
+          result += "<td><button type='button' class='btn btn-primary' onClick='selectNewDay(" + "workingYear, workingMonth, " + dayNum + ")'>" + dayNum + "</button></td>";
+        } else {
+          result += "<td><button type='button' class='btn btn-light' onClick='selectNewDay(" + "workingYear, workingMonth, " + dayNum + ")'>" + dayNum + "</button></td>";
+        }
 			}
 		}
 		result += "</tr>";
@@ -61,30 +92,27 @@ function buildBreakdownHeader() {
 }
 
 function buildBreakdownContent() {
-  fetch(eventsLocation).then((response) => response.json()).then((data) => {
-    let element = document.getElementById("dayBreakdownContent");
-    let contents = "";
-    console.log(data[0].times);
-    for (let i = 0; i < 24; i++) {
-      contents += "<tr>";
-      let dispNum = (12+i)%12;
-      dispNum = dispNum == 0 ? 12 : dispNum;
-      let dispSuffix = (12/i) > 1 ? "AM" : "PM";
-      contents += "<td>" + dispNum + ":00" + dispSuffix + "</td>"
-      if (i < data.length) {
-        let dateInfo = String(data[i].date).split("-");
-        if (dateEqualsSelected(dateInfo) && data[i].times.includes(i)) {
-          contents += "<td colspan='2'>" + data[i].name + "</td>"
-        } else {
-          contents += "<td colspan='2'></td>";
-        }
-      } else {
-        contents += "<td colspan='2'></td>";
+  let element = document.getElementById("dayBreakdownContent");
+  let contents = "";
+  for (let i = 0; i < 24; i++) {
+    contents += "<tr>";
+    
+    let dispNum = (12+i)%12;
+    dispNum = dispNum == 0 ? 12 : dispNum;
+    let dispSuffix = (12/i) > 1 ? "AM" : "PM";
+    
+    contents += "<td>" + dispNum + ":00" + dispSuffix + "</td>"
+    
+    contents += "<td>"
+    for (let j = 0; j < events.length; j++) {
+      let dateInfo = String(events[j].date).split("-");
+      if (dateEqualsSelected(dateInfo) && events[j].times.includes(i)) {
+        contents += "<p>" + events[j].person + " - " + events[j].name + "</p>"
       }
-      contents += "</tr>"
     }
-    element.innerHTML = contents;
-  });
+    contents += "</td></tr>"
+  }
+  element.innerHTML = contents;
 }
 
 function parseEvents(year, month) {
@@ -120,23 +148,30 @@ function buildTasksHeader() {
 }
 
 function buildTasksBody() {
-  data = JSON.parse(localStorage.getItem('chores'));
-  let element = document.getElementById("tasksBody");
-  let contents = "";
-  for (let i = 0; i < data.length; i++) {
-    let dateInfo = String(data[i].date).split("-");
-    if (dateEqualsSelected(dateInfo)) {
-      contents += "<tr>";
-      contents += "<td>" + data[i]["chore-description"] + "</td>";
-      contents += "<td>" + data[i].person + "</td>";
-      contents += "</tr>";
+  fetch(tasksLocation).then((response) => response.json()).then((data) => {
+    let element = document.getElementById("tasksBody");
+    let contents = "";
+    for (let i = 0; i < data.length; i++) {
+      let dateInfo = String(data[i].date).split("-");
+      if (dateEqualsSelected(dateInfo)) {
+        contents += "<tr>";
+        contents += "<td>" + data[i]["chore-description"] + "</td>";
+        contents += "<td>" + data[i].person + "</td>";
+        contents += "</tr>";
+      }
     }
-  }
-  element.innerHTML = contents;
+    element.innerHTML = contents;
+  });
 }
 
 function dateEqualsSelected(d) {
   return d[0] == selectedDate.year && d[1] == selectedDate.month+1 && d[2] == selectedDate.day;
+}
+
+function jumpTo() {
+  workingYear = selectedDate.year;
+  workingMonth = selectedDate.month;
+  init();
 }
 
 function init() {
